13001f07451b7f0a64d33a5379f25488ea3d2976
Andrei Dzebysh <presnia@yahoo.com>
2021-05-29 14:52:51 +0300

    Added functionality to the button

--- js/index.js
+++ js/index.js
@@ -1,20 +1,7 @@
-(function () {
-  var a = (b = 5);
-})();
-console.log(b);
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
 
-(function () {
-  console.log(1);
-  setTimeout(() => console.log(2), 1000);
-  setTimeout(() => console.log(3), 0);
-  Promise.resolve(true).then(() => console.log(4));
-  console.log(5);
-})();
-
-try {
-  setTimeout(function () {
-    throw new Error();
-  }, 1000);
-} catch (e) {
-  console.log(e);
-}
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+});
